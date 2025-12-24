3676631a5627ad42134698db7af6735fe097cb9e
app-kaisha <app.kaihatsusha@gmail.com>
2025-12-24 20:45:32 +0000

    Initial Commit

---
+++ Elvis/ElvisApp.swift
@@ -0,0 +1,18 @@
+//
+//  ElvisApp.swift
+//  Elvis
+//
+//  Created by app-kaihatsusha on 24/12/2025.
+//  Copyright © 2025 app-kaihatsusha. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct ElvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
